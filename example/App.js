f476272f147b0df2d6275eac2b2c33c6b8a2271f
Thomas Schoffelen <thomas@schof.co>
2020-09-16 09:26:52 +0100

    Rewrote package to use hooks rather than class components.

--- example/App.js
+++ example/App.js
@@ -1,40 +1,46 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet, Button } from 'react-native'
-import { KeycodeInput } from 'react-native-keycode'
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { KeycodeInput } from './index';
 
-export default class App extends Component {
-  state = {
-    value: ''
-  }
+const App = () => {
+  const [value, setValue] = useState('');
+  const [numeric, setNumeric] = useState(false);
+  return (
+    <View style={styles.container}>
 
-  render () {
-    return (
-      <View style={styles.container}>
+      <Text style={styles.text}>Enter your access code</Text>
 
-        <Text style={styles.text}>Enter your access code.</Text>
+      <KeycodeInput
+        numeric={numeric}
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
+        onComplete={(completedValue) => {
+          alert('Completed! Value: ' + completedValue);
+        }}/>
 
-        <KeycodeInput
-          value={this.state.value}
-          onChange={(value) => this.setState({value})}
-          onComplete={(value) => {
-            alert('Completed! Value: ' + value)
+      <View style={styles.button}>
+        <Button
+          color='#9b9b9b'
+          title='Set value'
+          onPress={() => setValue('33')}/>
+        <Button
+          color='#9b9b9b'
+          title='Reset value'
+          onPress={() => setValue('')}/>
+        <Button
+          color='#9b9b9b'
+          title={!numeric ? 'Switch to numeric mode' : 'Switch to alpha mode'}
+          onPress={() => {
+            setValue('');
+            setNumeric(!numeric);
           }}/>
-
-        <View style={styles.button}>
-          <Button
-            color='#9b9b9b'
-            title='Set value'
-            onPress={() => this.setState({value: '3F'})}/>
-          <Button
-            color='#9b9b9b'
-            title='Reset value'
-            onPress={() => this.setState({value: ''})}/>
-        </View>
-
       </View>
-    )
-  }
-}
+
+    </View>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -51,4 +57,4 @@ const styles = StyleSheet.create({
   button: {
     marginTop: 96
   }
-})
+});
--- index.js
+++ index.js
@@ -4,159 +4,136 @@
  * This file supports both iOS and Android.
  */
 
-import React, { Component } from 'react'
-import { View, TextInput, Text, StyleSheet, Platform } from 'react-native'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Text, StyleSheet, Platform } from 'react-native';
+import PropTypes from 'prop-types';
 
-export class KeycodeInput extends Component {
-  static propTypes = {
-    length: PropTypes.number,
-    tintColor: PropTypes.string,
-    textColor: PropTypes.string,
-    onChange: PropTypes.func,
-    onComplete: PropTypes.func,
-    autoFocus: PropTypes.bool,
-    defaultValue: PropTypes.string,
-    regex: PropTypes.any,
-    uppercase: PropTypes.bool,
-    alphaNumeric: PropTypes.bool,
-    numeric: PropTypes.bool,
-    value: PropTypes.string,
-    style: PropTypes.any,
-    inputRef: PropTypes.func
+export const KeycodeInput = (props) => {
+  const [inputValue, setInputValue] = useState(props.defaultValue);
+  useEffect(() => {
+    if (props.value !== undefined && props.value !== inputValue) {
+      setInputValue(props.value);
+    }
+  }, [props.value]);
+
+  if (props.value !== undefined && !props.onChange) {
+    throw new Error(
+      'To use the KeycodeInput as a controlled component, ' +
+      'you need to supply both the value and onChange props.'
+    );
   }
 
-  static defaultProps = {
-    tintColor: '#007AFF',
-    textColor: '#000',
-    length: 4,
-    autoFocus: true,
-    numeric: false,
-    alphaNumeric: true,
-    uppercase: true,
-    defaultValue: '',
-    value: ''
-  }
-
-  input = null
-
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      value: props.defaultValue
+  const changeText = (value) => {
+    if (props.uppercase) {
+      value = value.toUpperCase();
     }
-  }
-
-  async componentWillReceiveProps (nextProps) {
-    if ('value' in nextProps && nextProps.value !== this.state.value) {
-      await this._setValue(nextProps.value)
+    if (props.alphaNumeric) {
+      value = value.replace('/[^a-z0-9]/i', '');
     }
 
-    if (this.state.value.length < this.props.length) {
-      this.props.autoFocus && this.input.focus()
-    } else {
-      this.input.blur()
-    }
-  }
+    setInputValue(value);
 
-  _setValue (value) {
-    if (this.props.uppercase) {
-      value = value.toUpperCase()
-    }
-    if (this.props.alphaNumeric) {
-      value = value.replace('/[^a-z0-9]/i', '')
+    if (props.onChange) {
+      props.onChange(value);
     }
 
-    this.setState({value})
-  }
-
-  async _changeText (value) {
-    await this._setValue(value)
-
-    if (this.props.onChange) {
-      await this.props.onChange(this.state.value)
+    if (value.length < props.length) {
+      return;
     }
 
-    if (this.state.value.length < this.props.length) {
-      return
+    if (props.onComplete) {
+      props.onComplete(value);
     }
+  };
 
-    if (this.input) {
-      this.input.blur()
-    }
-
-    if (this.props.onComplete) {
-      setTimeout(() => {
-        this.props.onComplete(this.state.value)
-      }, 250)
-    }
-  }
-
-  _renderBoxes () {
-    let elements = []
-    let i = 0
-    let vals = this.state.value.split('')
-    while (i < this.props.length) {
-      let active = i === this.state.value.length
-      let barStyles = [styles.bar, active ? [styles.barActive, {backgroundColor: this.props.tintColor}] : []]
+  const renderBoxes = () => {
+    let elements = [];
+    let i = 0;
+    let vals = inputValue.split('');
+    while (i < props.length) {
+      let active = i === inputValue.length;
+      let barStyles = [styles.bar, active ? [styles.barActive, { backgroundColor: props.tintColor }] : []];
 
       elements.push(
         <View style={styles.box} key={i}>
-          <Text style={[styles.text, { color: this.props.textColor }]}>{vals[i] || ''}</Text>
+          <Text style={styles.text}>{vals[i] || ''}</Text>
           <View style={barStyles}/>
         </View>
-      )
+      );
 
-      i++
+      i++;
     }
 
-    return elements
-  }
+    return elements;
+  };
 
-  render () {
-    let keyboardType = this.props.numeric ? 'numeric' : (Platform.OS === 'ios' ? 'ascii-capable' : 'default')
+  let keyboardType = props.numeric ? 'numeric' : (Platform.OS === 'ios' ? 'ascii-capable' : 'default');
 
-    return (
-      <View style={[styles.container, this.props.style]}>
-        <TextInput
-          ref={(component) => {
-            this.input = component
-            if (this.props.inputRef) {
-              this.props.inputRef(component)
-            }
-          }}
-          style={styles.input}
-          autoFocus={this.props.autoFocus}
-          autoCorrect={false}
-          autoCapitalize='characters'
-          value={this.state.value}
-          blurOnSubmit={false}
-          keyboardType={keyboardType}
-          maxLength={this.props.length}
-          disableFullscreenUI
-          clearButtonMode='never'
-          spellCheck={false}
-          returnKeyType='go'
-          underlineColorAndroid='transparent'
-          onChangeText={(text) => this._changeText(text)}
-          caretHidden/>
+  return (
+    <View style={[styles.container, props.style]}>
+      {renderBoxes()}
+      <TextInput
+        ref={(component) => {
+          if (props.inputRef) {
+            props.inputRef(component);
+          }
+        }}
+        style={[styles.input, { color: props.textColor, width: 42 * props.length }]}
+        autoFocus={props.autoFocus}
+        autoCorrect={false}
+        autoCapitalize='characters'
+        value={inputValue}
+        blurOnSubmit={false}
+        keyboardType={keyboardType}
+        maxLength={props.length}
+        disableFullscreenUI
+        clearButtonMode='never'
+        spellCheck={false}
+        returnKeyType='go'
+        underlineColorAndroid='transparent'
+        onChangeText={(text) => changeText(text)}
+        caretHidden/>
+    </View>
+  );
+};
 
-        {this._renderBoxes()}
-      </View>
-    )
-  }
-}
+KeycodeInput.propTypes = {
+  length: PropTypes.number,
+  tintColor: PropTypes.string,
+  textColor: PropTypes.string,
+  onChange: PropTypes.func,
+  onComplete: PropTypes.func,
+  autoFocus: PropTypes.bool,
+  uppercase: PropTypes.bool,
+  alphaNumeric: PropTypes.bool,
+  numeric: PropTypes.bool,
+  value: PropTypes.string,
+  style: PropTypes.any,
+  inputRef: PropTypes.func
+};
+
+KeycodeInput.defaultProps = {
+  tintColor: '#007AFF',
+  textColor: '#000',
+  length: 4,
+  autoFocus: true,
+  numeric: false,
+  alphaNumeric: true,
+  uppercase: true,
+  defaultValue: ''
+};
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    position: 'relative'
   },
   input: {
-    width: 0,
-    height: 0,
-    opacity: 0
+    height: 48,
+    position: 'absolute',
+    opacity: 0,
+    zIndex: 100
   },
   box: {
     width: 32,
@@ -180,4 +157,4 @@ const styles = StyleSheet.create({
     width: 32,
     marginBottom: 8
   }
-})
+});
